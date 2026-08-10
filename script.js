de011ca7aba718ba3a70e66caac76475333cda19
rishabh-2714 <rishabh.2714@bis.edu.in>
2026-08-10 17:16:15 +0530

    Added cycling property to <img> with id 'symbols'.

---
+++ script.js
@@ -0,0 +1,8 @@
+const imgSrcs=["assets/IndiaFlag.png", "assets/IndiaEmblem.png", "assets/BengalTiger.png", "assets/Peacock.png", 
+    "assets/Lotus.png", "assets/HapusMango.png", "assets/Banyan.png", "assets/IndianElephant.png"
+];
+let i=0;
+function cycleImages() {
+    i=(i<imgSrcs.length?i+1:0);
+    document.getElementById('symbols').src=imgSrcs[i];
+}
